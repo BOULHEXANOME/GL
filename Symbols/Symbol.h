cd9dc4e6d36ce5f5776be8979cb184f2c9cb9970
HugoDelval <hugo.delval@insa-lyon.fr>
2016-03-15 19:49:37 +0100

    introduction of lines and columns

--- Symbols/Symbol.h
+++ Symbols/Symbol.h
@@ -8,11 +8,42 @@
 #include "../symbolTypes.h"
 #include <iostream>
 
-class Symbol {
+class Symbol
+{
+protected:
+    int columnWhereSymbolOccurs = -1;
+    int lineWhereSymbolOccurs = -1;
 
 public:
     virtual int getType() const;
     virtual std::string print() const;
+
+    /***********************/
+    /*** Getters/Setters ***/
+    /***********************/
+    int getColumnWhereSymbolOccurs() const
+    {
+        return columnWhereSymbolOccurs;
+    }
+
+    void setColumnWhereSymbolOccurs(int columnWhereSymbolOccurs)
+    {
+        Symbol::columnWhereSymbolOccurs = columnWhereSymbolOccurs;
+    }
+
+    int getLineWhereSymbolOccurs() const
+    {
+        return lineWhereSymbolOccurs;
+    }
+
+    void setLineWhereSymbolOccurs(int lineWhereSymbolOccurs)
+    {
+        Symbol::lineWhereSymbolOccurs = lineWhereSymbolOccurs;
+    }
+
+    /***********************/
+    /* End Getters/Setters */
+    /***********************/
 
     /************************/
     /** Override operators **/
