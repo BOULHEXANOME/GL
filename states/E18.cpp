96f12115706a343fdc16f084db1cdc5f5bf9ec14
Hugo Sipahimalani = fuck Benoit Chabod <hugo.sipahi@hotmail.fr>
2016-03-23 14:33:06 +0100

    transitions for states 16 to 20 are verified

--- states/E18.cpp
+++ states/E18.cpp
@@ -3,6 +3,9 @@
 //
 
 #include "E18.h"
+#include "E21.h"
+#include "E38.h"
+#include "E39.h"
 #include "../Symbols/Nonterminaux/Write.h"
 
 E18::E18()
@@ -27,4 +30,19 @@ bool E18::transitionSemicolon(Automaton *automaton, Symbol *semicolon)
     automaton->popState();
     (*automaton->states.begin())->transition(automaton, e);
     return true;
+}
+
+bool E18::transitionOpA(Automaton * automaton, Symbol * opa){
+	automaton->pushState(opa, new E21());
+	return true;
+}
+
+bool E18::transitionPlus(Automaton * automaton, Symbol * plus){
+	automaton->pushState(plus, new E38());
+	return true;
+}
+
+bool E18::transitionMinus(Automaton * automaton, Symbol * minus){
+	automaton->pushState(minus, new E39());
+	return true;
 }
--- states/E18.h
+++ states/E18.h
@@ -16,8 +16,10 @@ protected:
 public:
     E18();
     ~E18(){};
-    // FIXME transitions
     virtual bool transitionSemicolon(Automaton * automaton, Symbol * semicolon);
+    virtual bool transitionOpA(Automaton * automaton, Symbol * opa);
+    virtual bool transitionPlus(Automaton * automaton, Symbol * plus);
+    virtual bool transitionMinus(Automaton * automaton, Symbol * minus);
 
 };
 
